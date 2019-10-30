f3f7e897df8fd3ff3ba1169f68ea951b1a597982
Adrienne Dreyfus <adreyfus@stripe.com>
2019-10-30 15:34:39 -0700

    Renamed to use publishable key and local env

--- client-and-server/server/node/server.js
+++ client-and-server/server/node/server.js
@@ -3,7 +3,6 @@ const app = express();
 const { resolve } = require("path");
 // Copy the .env.example in the root into a .env file in this folder
 
-
 const env = require("dotenv").config({ path: "./.env" });
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
