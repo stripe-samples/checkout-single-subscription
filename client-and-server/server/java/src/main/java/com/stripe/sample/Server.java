b67a13217a7f9d7b76952e2f9a3b7ada5d68302b
Adrienne Dreyfus <adreyfus@stripe.com>
2019-10-30 15:28:36 -0700

    Renamed to use publishable key and local env

--- client-and-server/server/java/src/main/java/com/stripe/sample/Server.java
+++ client-and-server/server/java/src/main/java/com/stripe/sample/Server.java
@@ -38,8 +38,8 @@ public class Server {
 
     public static void main(String[] args) {
         port(4242);
-        String ENV_PATH = "../../../";
-        Dotenv dotenv = Dotenv.configure().directory(ENV_PATH).load();
+        
+        Dotenv dotenv = Dotenv.load();
 
         Stripe.apiKey = dotenv.get("STRIPE_SECRET_KEY");
 
@@ -50,7 +50,7 @@ public class Server {
             response.type("application/json");
 
             Map<String, Object> responseData = new HashMap<>();
-            responseData.put("publicKey", dotenv.get("STRIPE_PUBLIC_KEY"));
+            responseData.put("publicKey", dotenv.get("STRIPE_PUBLISHABLE_KEY"));
             responseData.put("basicPlan", dotenv.get("BASIC_PLAN_ID"));
             responseData.put("proPlan", dotenv.get("PRO_PLAN_ID"));
             return gson.toJson(responseData);
