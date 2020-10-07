ec1648712c97f5167e5e01ae26530ba040f4fcf4
Mikito Takada <mikito@stripe.com>
2020-10-06 17:23:16 -0700

    Instead of searching for the env file relative to the current working directory (which may fail if the user runs npm start outside the server dir), always look for it relative to where server.js is on the file system

--- server/node/server.js
+++ server/node/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // Copy the .env.example in the root into a .env file in this folder
-const envFilePath = path.resolve('./.env');
+const envFilePath = path.resolve(__dirname, './.env');
 const env = require("dotenv").config({ path: envFilePath });
 if (env.error) {
   throw new Error(`Unable to load the .env file from ${envFilePath}. Please copy .env.example to ${envFilePath}`);
