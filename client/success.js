c45ddb139b143293e410ff40bd3ff2ec870fbe87
CJ Avilla <cjavilla@stripe.com>
2021-06-16 13:32:21 -0700

    Remove mention of customer ID from success page

--- client/success.js
+++ client/success.js
@@ -1,6 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 const sessionId = urlParams.get("session_id")
-let customerId;
 
 if (sessionId) {
   fetch("/checkout-session?sessionId=" + sessionId)
@@ -8,15 +7,6 @@ if (sessionId) {
       return result.json()
     })
     .then(function(session){
-      // We store the customer ID here so that we can pass to the
-      // server and redirect to customer portal. Note that, in practice
-      // this ID should be stored in your database when you receive
-      // the checkout.session.completed event. This demo does not have
-      // a database, so this is the workaround. This is *not* secure.
-      // You should use the Stripe Customer ID from the authenticated
-      // user on the server.
-      customerId = session.customer;
-
       var sessionJSON = JSON.stringify(session, null, 2);
       document.querySelector("pre").textContent = sessionJSON;
     })
@@ -25,7 +15,7 @@ if (sessionId) {
     });
 
   // In production, this should check CSRF, and not pass the session ID.
-  // The customer ID for the portal should be pulled from the 
+  // The customer ID for the portal should be pulled from the
   // authenticated user on the server.
   const manageBillingForm = document.querySelector('#manage-billing-form');
   manageBillingForm.addEventListener('submit', function(e) {
