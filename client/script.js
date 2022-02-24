a87fc2fcb78d3c1c56c5e1707cb6e746e8376f65
CJ Avilla <cjavilla@stripe.com>
2022-02-24 12:20:40 -0500

    Remove unused code
    Fixes #236

--- client/script.js
+++ client/script.js
@@ -7,24 +7,3 @@ fetch("/config")
     const proPriceInput = document.querySelector('#proPrice');
     proPriceInput.value = proPrice;
   })
-
-// If a fetch error occurs, log it to the console and show it in the UI.
-var handleFetchResult = function(result) {
-  if (!result.ok) {
-    return result.json().then(function(json) {
-      if (json.error && json.error.message) {
-        throw new Error(result.url + ' ' + result.status + ' ' + json.error.message);
-      }
-    }).catch(function(err) {
-      showErrorMessage(err);
-      throw err;
-    });
-  }
-  return result.json();
-};
-
-var showErrorMessage = function(message) {
-  var errorEl = document.getElementById("error-message")
-  errorEl.textContent = message;
-  errorEl.style.display = "block";
-};
